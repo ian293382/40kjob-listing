df93f5b4863d163c1e74455cab88f947b9672430
ian293382 <moshou5571228@gmail.com>
2020-05-15 14:59:15 +0800

    fix index

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,12 +1,11 @@
 module FlashesHelper
-  FLASH_CLASS = { alert: "danger", notice: "success", warming: "warning"}.freeze
+  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
   def flash_class(key)
-    FLASH_CLASS.fetch key.to_sym, key
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
-  def user_facting_flashes
-    flash.to_hash.slice "alert", "notice", "warning"
+  def user_facing_flashes
+    flash.to_hash.slice "alert", "notice","warning"
   end
-
 end
