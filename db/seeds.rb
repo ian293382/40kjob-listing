c8c3056707facb3fa8e176965a97bf7a91747403
Ian293382 <moshou5571228@gmail.com>
2022-04-15 01:03:06 +0800

    add admin_to_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,13 @@
     Location.create!(name: "Kaohsiung")
 
     Location.create!(name: "Ａbroad")
+
+    u = User.new
+    u.email = "123@qwe.com"           # 可以改成自己的 email
+
+    u.password = "123qwe"                # 最少要六码
+
+    u.password_confirmation = "123qwe"   # 最少要六码
+
+    u.is_admin = true
+    u.save
